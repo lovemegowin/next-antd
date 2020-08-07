46f41924692b42bb2df07e2254e62a8feaea6c92
Luke Skywalker <lovemegowin@gmail.com>
2020-08-07 16:01:24 +0800

    initial

---
+++ components/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styles from './index.module.scss'
+import {Button} from "antd";
+
+
+const Index = () => {
+    return <div className={styles.container}>
+        <Button type={'primary'}>Button</Button>
+    </div>
+}
+
+export default Index
